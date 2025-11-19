256d05930be02bfb9b78722ff9dac2ff545061d4
Autumntumu <ponkrit180548@gmail.com>
2025-11-19 20:56:33 +0700

    Final

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware   
 from supabase_client import get_advice_by_type
 from inference_sdk import InferenceHTTPClient
 import tempfile
 
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Roboflow Configuration ---
 client = InferenceHTTPClient(
@@ -12,28 +20,30 @@ client = InferenceHTTPClient(
     api_key="sLue4JoTA2R66Awyfohx"
 )
 
-WORKSPACE_NAME = "beoseu"           
-WORKFLOW_ID = "custom-workflow"
+WORKSPACE_NAME = "beoseu"
+WORKFLOW_ID = "detect-count-and-visualize"
 
 
 # --- API Route ---
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
     try:
-        # Sauvegarder temporairement l'image reçue du Flutter
         with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp:
             temp.write(await file.read())
             temp_path = temp.name
 
-        # Appeler ton workflow Roboflow
         result = client.run_workflow(
-            workspace_name=WORKSPACE_NAME,
-            workflow_id=WORKFLOW_ID,
-            images={"image": temp_path},
-            use_cache=True
-        )
+    workspace_name=WORKSPACE_NAME,
+    workflow_id=WORKFLOW_ID,
+    images={"image": temp_path},
+    use_cache=True
+)
+        
+        data = result[0]
 
-        data = result[0]  
+
+        output_image_b64 = data.get("output_image")
+
         predictions_dict = data["predictions"]
         detections = predictions_dict["predictions"]
         classes_detected = [d["class"] for d in detections]
@@ -41,11 +51,12 @@ async def upload_image(file: UploadFile = File(...)):
         advice_dict = {}
         for acne_type in classes_detected:
             advice_dict[acne_type] = get_advice_by_type(acne_type)
-        print(advice_dict)
+
         return {
             "status": "ok",
             "detected_types": list(classes_detected),
-            "advice": advice_dict
+            "advice": advice_dict,
+            "output_image": output_image_b64,   # 👈 ส่งกลับไปด้วย
         }
 
     except Exception as e:
